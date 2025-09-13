6c2bf20194ef0f9976299c81ac2ec15a5f027d81
tigrankhachatryan4 <tigran4484@yahoo.com>
2025-09-13 00:38:30 -0700

    sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const CORE_ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
